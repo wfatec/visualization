00480c3a95857d878288e8c820f4b9e9dea661f8
wfatec <chao125010@163.com>
2020-07-07 15:22:28 +0800

    draw shapes with parametric

---
+++ common/lib/parametric.js
@@ -0,0 +1,48 @@
+/*
+ * @Author: your name
+ * @Date: 2020-07-07 15:18:21
+ * @LastEditTime: 2020-07-07 15:19:22
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /visualization/common/lib/parametric.js
+ */
+
+function draw(
+  points,
+  context,
+  { strokeStyle = 'black', fillStyle = null, close = false } = {}
+) {
+  context.strokeStyle = strokeStyle;
+  context.beginPath();
+  context.moveTo(...points[0]);
+  for (let i = 1; i < points.length; i++) {
+    context.lineTo(...points[i]);
+  }
+  if (close) context.closePath();
+  if (fillStyle) {
+    context.fillStyle = fillStyle;
+    context.fill();
+  }
+  context.stroke();
+}
+
+export function parametric(sFunc, tFunc, rFunc) {
+  return function (start, end, seg = 100, ...args) {
+    const points = [];
+    for (let i = 0; i <= seg; i++) {
+      const p = i / seg;
+      const t = start * (1 - p) + end * p;
+      const x = sFunc(t, ...args);
+      const y = tFunc(t, ...args);
+      if (rFunc) {
+        points.push(rFunc(x, y));
+      } else {
+        points.push([x, y]);
+      }
+    }
+    return {
+      draw: draw.bind(null, points),
+      points,
+    };
+  };
+}
